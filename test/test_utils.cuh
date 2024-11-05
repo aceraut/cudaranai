9b658a813ac557e8ae46ccb2d35ee2f9512c2bb4
geckoil <73975841+geckoil@users.noreply.github.com>
2024-11-05 11:51:59 +0700

    Fix test case of maxpool

--- test/test_utils.cuh
+++ test/test_utils.cuh
@@ -1,24 +1,21 @@
 #pragma once
 
 #include <cassert>
-#include <cmath>
 #include <iostream>
+#include <math>
 #include <vector>
 
 #include <thrust/copy.h>
 #include <thrust/device_vector.h>
 
-// for testing, error margin doesn't need to be that small
+// For testing, error margin doesn't need to be that small
 static constexpr float ERROR_MARGIN = 0.00001;
 
 template <typename T>
 void print_vec(const thrust::device_vector<T> &vec) {
     std::cout << "[";
     thrust::copy(vec.begin(), vec.end(),
-                 std::ostream_iterator<T>(std::cout, ", "));
-    // for (const T &e : vec) {
-    //     std::cout << e << " ";
-    // }
+                 std::ostream_iterator<T>(std::cout, " "));
     std::cout << "\b]" << std::endl;
 }
 
@@ -35,8 +32,7 @@ template <typename T>
 void check_equal_vecs(const thrust::device_vector<T> &u,
                       const std::vector<T> &v) {
     assert(u.size() == v.size() && "size mismatch between vectors");
-
     for (int i = 0; i < u.size(); i++) {
-        assert(fabs(u[i] - v[i]) < ERROR_MARGIN && "Incorrect element");
+        assert(std::abs(u[i] - v[i]) < ERROR_MARGIN && "Incorrect element");
     }
 }
