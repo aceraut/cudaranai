d06698759aa6258bc92ffc03e1df162c7b62ae0b
geckoil <73975841+geckoil@users.noreply.github.com>
2024-12-23 19:52:58 +0700

    Rewrite maxpool kernel functions to avoid launching too many kernels

--- core/maxpool.cu
+++ core/maxpool.cu
@@ -12,7 +12,6 @@ namespace nnv2 {
 // largest value in every smaller local patches of the input feature maps and
 // place them in the output feature maps.
 __global__ void maxpool_forward_kernel(
-    int size,
     float *output,
     int *max_indices,
     const float *input,
@@ -25,45 +24,52 @@ __global__ void maxpool_forward_kernel(
     int stride_h,
     int stride_w,
     int out_h,
-    int out_w,
-    int in_stride,
-    int out_stride) {
+    int out_w) {
   // Each thread handles a pixel in the output image
-  CUDA_GRID_STRIDE_LOOP(idx, size) {
-    // Point to input and output images that this thread handles
-    int feat_idx = blockIdx.y;
-    input += feat_idx * in_stride;
-    output += feat_idx * out_stride;
-    max_indices += feat_idx * out_stride;
+  // Point to input and output images that this thread handles
+  int feat_idx = blockIdx.x;
+  input += feat_idx * in_h * in_w;
+  output += feat_idx * out_h * out_w;
+  max_indices += feat_idx * out_h * out_w;
 
-    // Coord in output image
-    int out_x = (idx / out_w) % out_h;
-    int out_y = idx % out_w;
+  int out_x_start = blockIdx.y * blockDim.y + threadIdx.y;
+  int out_y_start = threadIdx.x;
 
-    // Locate the base coords of the section in the input image that
-    // affects (out_x, out_y) of the output image
-    int in_x_start = out_x * stride_h - pad_h;
-    int in_y_start = out_y * stride_w - pad_w;
-    int in_x_end = fminf(in_x_start + filter_h, in_h);
-    int in_y_end = fminf(in_y_start + filter_w, in_w);
-    in_x_start = fmaxf(in_x_start, 0);
-    in_y_start = fmaxf(in_y_start, 0);
+  int out_x_stride = gridDim.y * blockDim.y;
+  int out_y_stride = blockDim.x;
 
-    // Loop over the local patch and select the pixel with largest value
-    float max_val = -FLT_MAX;
-    float max_idx = -1;
+  for (int out_x = out_x_start; out_x < out_h; out_x += out_x_stride) {
+    for (int out_y = out_y_start; out_y < out_w; out_y += out_y_stride) {
+      // Locate the base coords of the section in the input image that
+      // affects (out_x, out_y) of the output image
+      int in_x_start = out_x * stride_h - pad_h;
+      int in_y_start = out_y * stride_w - pad_w;
 
-    for (int in_x = in_x_start; in_x < in_x_end; in_x++) {
-      for (int in_y = in_y_start; in_y < in_y_end; in_y++) {
-        int in_idx = in_x * in_w + in_y;
-        if (input[in_idx] > max_val) {
-          max_idx = in_idx;
-          max_val = input[in_idx];
+      int in_x_end = in_x_start + filter_h;
+      in_x_end = in_x_end > in_h ? in_h : in_x_end;
+      int in_y_end = in_y_start + filter_w;
+      in_y_end = in_y_end > in_w ? in_w : in_y_end;
+
+      in_x_start = in_x_start < 0 ? 0 : in_x_start;
+      in_y_start = in_y_start < 0 ? 0 : in_y_start;
+
+      // Loop over the local patch and select the pixel with largest value
+      int idx = out_x * out_w + out_y;
+      float max_val = -FLT_MAX;
+      float max_idx = -1;
+
+      for (int in_x = in_x_start; in_x < in_x_end; in_x++) {
+        for (int in_y = in_y_start; in_y < in_y_end; in_y++) {
+          int in_idx = in_x * in_w + in_y;
+          if (input[in_idx] > max_val) {
+            max_idx = in_idx;
+            max_val = input[in_idx];
+          }
         }
       }
+      output[idx] = max_val;
+      max_indices[idx] = max_idx;
     }
-    output[idx] = max_val;
-    max_indices[idx] = max_idx;
   }
 }
 
@@ -95,19 +101,18 @@ void maxpool_forward(
 
   int in_h = input_shape[2];
   int in_w = input_shape[3];
-  int in_stride = in_h * in_w;
   int out_h = output_shape[2];
   int out_w = output_shape[3];
-  int size = out_h * out_w; // is also out_stride
 
   float *output_raw = RAW_PTR(output->get_vec());
   int *indices_raw = RAW_PTR(indices);
   const float *input_raw = RAW_PTR(input->get_vec());
 
-  dim3 grid_dim(utils::div_ceil(size, BLOCK_SIZE), batch_size * in_feats);
+  int block_num = std::max(1, 16 / in_feats);
+  dim3 grid_dim(batch_size * in_feats, block_num);
+  dim3 block_dim(32, 8);
 
-  maxpool_forward_kernel<<<grid_dim, BLOCK_SIZE>>>(
-      size,
+  maxpool_forward_kernel<<<grid_dim, block_dim>>>(
       output_raw,
       indices_raw,
       input_raw,
@@ -120,50 +125,66 @@ void maxpool_forward(
       stride_h,
       stride_w,
       out_h,
-      out_w,
-      in_stride,
-      size);
+      out_w);
   CUDA_POST_KERNEL_CHECK;
 }
 
 // Each output gradient is propagated back to its pooled position in the input.
 __global__ void maxpool_backward_kernel(
-    int size,
     float *input_grad,
     const float *output_grad,
     const int *max_indices,
-    int in_stride,
-    int out_stride) {
-  CUDA_GRID_STRIDE_LOOP(idx, size) {
-    int feat_idx = blockIdx.y;
-    input_grad += feat_idx * in_stride;
-    output_grad += feat_idx * out_stride;
-    max_indices += feat_idx * out_stride;
+    int in_h,
+    int in_w,
+    int out_h,
+    int out_w) {
+  int feat_idx = blockIdx.x;
+  input_grad += feat_idx * in_h * in_w;
+  output_grad += feat_idx * out_h * out_w;
+  max_indices += feat_idx * out_h * out_w;
 
-    int pooled_index = max_indices[idx];
-    float grad_value = output_grad[idx];
+  int out_x_start = blockIdx.y * blockDim.y + threadIdx.y;
+  int out_y_start = threadIdx.x;
 
-    input_grad[pooled_index] += grad_value;
+  int out_x_stride = gridDim.y * blockDim.y;
+  int out_y_stride = blockDim.x;
+
+  for (int out_x = out_x_start; out_x < out_h; out_x += out_x_stride) {
+    for (int out_y = out_y_start; out_y < out_w; out_y += out_y_stride) {
+      int idx = out_x * out_w + out_y;
+      int max_idx = max_indices[idx];
+
+      input_grad[max_idx] += output_grad[idx];
+    }
   }
 }
 
 __global__ void maxpool_backward_atomic_kernel(
-    int size,
     float *input_grad,
     const float *output_grad,
     const int *max_indices,
-    int in_stride,
-    int out_stride) {
-  CUDA_GRID_STRIDE_LOOP(idx, size) {
-    int feat_idx = blockIdx.y;
-    input_grad += feat_idx * in_stride;
-    output_grad += feat_idx * out_stride;
-    max_indices += feat_idx * out_stride;
+    int in_h,
+    int in_w,
+    int out_h,
+    int out_w) {
+  int feat_idx = blockIdx.x;
+  input_grad += feat_idx * in_h * in_w;
+  output_grad += feat_idx * out_h * out_w;
+  max_indices += feat_idx * out_h * out_w;
 
-    int pooled_index = max_indices[idx];
-    float grad_value = output_grad[idx];
+  int out_x_start = blockIdx.y * blockDim.y + threadIdx.y;
+  int out_y_start = threadIdx.x;
 
-    atomicAdd(&input_grad[pooled_index], grad_value);
+  int out_x_stride = gridDim.y * blockDim.y;
+  int out_y_stride = blockDim.x;
+
+  for (int out_x = out_x_start; out_x < out_h; out_x += out_x_stride) {
+    for (int out_y = out_y_start; out_y < out_w; out_y += out_y_stride) {
+      int idx = out_x * out_w + out_y;
+      int max_idx = max_indices[idx];
+
+      atomicAdd(&input_grad[max_idx], output_grad[idx]);
+    }
   }
 }
 
@@ -201,32 +222,27 @@ void maxpool_backward(
   CHECK_EQ(
       output_grad_shape[1], in_feats, "maxpool_backward: feature size error");
 
-  int in_stride = input_grad_shape[2] * input_grad_shape[3];
-  int out_stride = output_grad_shape[2] * output_grad_shape[3];
-  dim3 grid_dim(utils::div_ceil(out_stride, BLOCK_SIZE), batch_size * in_feats);
-
   float *input_grad_raw = RAW_PTR(input_grad->get_vec());
   const float *output_grad_raw = RAW_PTR(output_grad->get_vec());
   const int *indices_raw = RAW_PTR(indices);
 
+  int in_h = input_grad_shape[2];
+  int in_w = input_grad_shape[3];
+  int out_h = output_grad_shape[2];
+  int out_w = output_grad_shape[3];
+
+  int block_num = std::max(1, 16 / in_feats);
+  dim3 grid_dim(batch_size * in_feats, block_num);
+  dim3 block_dim(32, 8);
+
   // If stride dimension is the same as the filter dimension, gradient value
   // of pooled pixels in the input can be propagated without atomic expression
   if (filter_h == stride_h && filter_w == stride_w) {
-    maxpool_backward_kernel<<<grid_dim, BLOCK_SIZE>>>(
-        out_stride,
-        input_grad_raw,
-        output_grad_raw,
-        indices_raw,
-        in_stride,
-        out_stride);
+    maxpool_backward_kernel<<<grid_dim, block_dim>>>(
+        input_grad_raw, output_grad_raw, indices_raw, in_h, in_w, out_h, out_w);
   } else {
-    maxpool_backward_atomic_kernel<<<grid_dim, BLOCK_SIZE>>>(
-        out_stride,
-        input_grad_raw,
-        output_grad_raw,
-        indices_raw,
-        in_stride,
-        out_stride);
+    maxpool_backward_atomic_kernel<<<grid_dim, block_dim>>>(
+        input_grad_raw, output_grad_raw, indices_raw, in_h, in_w, out_h, out_w);
   }
   CUDA_POST_KERNEL_CHECK;
 }
